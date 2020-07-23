8c218f6b777d2e4bead32d2890be3dc9ceba42b1
Jeneth Diesta <jen6one9@gmail.com>
2020-07-22 22:13:01 -0700

    tested prompt question 1

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+//questions array
 var questions = [
     {
         prompt: "What is meta response tag?\n(a) Responsive tag for mobile devices\n\(b) A semantic element\n(c) html response",
         answer: "a"
     }
-var score = 0
 ]
+var score = 0;
+
+//for loop
+for (var i = 0; i < questions.length; i++) {
+    var response = window.prompt(questions[i].prompt);
+    if(response == questions[i].answer) {
+        score++;
+        alert("Correct!");
+    } else {
+        alert("Incorrect.");
+    }
+}
+    alert("you got" + score + "/" + questions.length.numberofquestions)
