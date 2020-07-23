378cfac8e313934d37e34968c939d9dd53dbdc8a
Jeneth Diesta <jen6one9@gmail.com>
2020-07-22 21:46:29 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var questions = [
+    {
+        prompt: "What is meta response tag?\n(a) Responsive tag for mobile devices\n\(b) A semantic element\n(c) html response",
+        answer: "a"
+    }
+var score = 0
+]
