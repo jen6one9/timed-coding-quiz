40b4f7ca9049c70d561d521d746b1d9876a66549
Jeneth Diesta <jen6one9@gmail.com>
2020-07-27 08:50:59 -0700

    updated the logic in JS

--- script.js
+++ script.js
@@ -4,33 +4,42 @@
 //After the declaration, the variable is empty (it has no value)
 
 //variables for the start of the quiz:
-var startBtn = document.querySelector("#click-here-to-begin");
-var pg1 = document.querySelector("#startQuiz");
-var pg2 = document.querySelector("#multChoice");
-var timer = document.querySelector("#timer");
-//variables for what happens if there time runs out
+var startBtn = document.querySelector("#quizStart");
 var timesUp = document.querySelector("#timesUp");
-var score = document.querySelector("showScore");
-//variables for user's score
-var pg3 = document.querySelector("#score");
-var saveUserScore = document.querySelector("#saveUserScore");
-var initials = document.querySelector("#userInt");
-var score = document.querySelector("#scoreResults");
-//variables for the highest score and quiz restart
-var pg4 = document.querySelector("#highestScore");
-var userinitials = document.querySelector("#initials");
-var restartQuiz = document.querySelector("#restartQuiz");
-//variables for the mult choice quiz
-var problem = document.querySelector("#question");
-var multiA = document.querySelector("#choiceA");
-var multiB = document.querySelector("#choiceB");
-var multiC = document.querySelector("#choiceC");
-var multiD = document.querySelector("#choiceD");
-var finalScore = document.querySelector("finalResultsMsg");
-//quiz questions 
+var quiz = document.querySelector("#quizContainer");
+var problem = document.querySelector("#questions");
+var multiA = document.querySelector("#A");
+var multiB = document.querySelector("#B");
+var multiC = document.querySelector("#C");
+var multiD = document.querySelector("#D");
+var score = document.querySelector("#score");
+timesUp.style.display= "none";
+quiz.style.display= "none";
+// var pg1 = document.querySelector("#startQuiz");
+// var pg2 = document.querySelector("#multChoice");
+// var timer = document.querySelector("#timer");
+// //variables for what happens if there time runs out
+
+// var score = document.querySelector("showScore");
+// //variables for user's score
+
+// var saveUserScore = document.querySelector("#saveUserScore");
+// var initials = document.querySelector("#userInt");
+// var score = document.querySelector("#scoreResults");
+// //variables for the highest score and quiz restart
+// var pg4 = document.querySelector("#highestScore");
+// var userinitials = document.querySelector("#initials");
+// var restartQuiz = document.querySelector("#restartQuiz");
+// //variables for the mult choice quiz
+
+// var finalScore = document.querySelector("finalResultsMsg");
+// //quiz questions
+var correct = 0;
+var incorrect = 0; 
+var currentQuestion = 0;
 var buttonquestions = [
     {
-        question1: "What is the viewport meta tag?",
+        question: "What is the viewport meta tag?",
         choiceA: "Responsive tag for mobile devices",
         choiceB: "A semantic element",
         choiceC: "An html response",
@@ -40,7 +49,7 @@ var buttonquestions = [
 
 
     {
-        question2: "What does JSON stand for?",
+        question: "What does JSON stand for?",
         choiceA: "JavaScript Order Notation",
         choiceB: "JavaScript Object Notation",
         choiceC: "Jupiter Saturn Outer Nebula",
@@ -50,7 +59,7 @@ var buttonquestions = [
 
 
     {
-        question3: "Does every HTML page begin as static content?",
+        question: "Does every HTML page begin as static content?",
         choiceA: "No, not every HTML page begins as static content.",
         choiceB: "Yes, every HTML page begins as static content.",
         choiceC: "Sometimes, based on the content.",
@@ -60,7 +69,7 @@ var buttonquestions = [
 
 
     {
-        question4: "Methods that find elements in the DOM tree are called?",
+        question: "Methods that find elements in the DOM tree are called?",
         choiceA: "variables",
         choiceB: "queries",
         choiceC: "elements",
@@ -69,7 +78,7 @@ var buttonquestions = [
     },
 
     {
-        question5: "to transform block elements into in-line elements, we use a CSS property called?",
+        question: "to transform block elements into in-line elements, we use a CSS property called?",
         choiceA: "sidebar",
         choiceB: "float",
         choiceC: "layout",
@@ -79,51 +88,137 @@ var buttonquestions = [
 
 ];
 
-//Trigger the first question after the "Click to Begin"start button is clicked.
-var answer = "";
-console.log(answer);
-var seconds = 120;
-var correct = 0;
-var currentQuestion = 0;
-var timeInterval;
-
-
-document.getElementById("quizStart").addEventListener("click", function () {
-    pg1.setAttribute("style", "display:none");
-    console.log(pg1);
-    pg2.setAttribute("style", "display:block");
-    correct = 0;
-    currentQuestion = 0;
-    countdown();
-    displayQuestion();
-});
-console.log("quizStart")
-
-
-
-
-//for timer countdown:
-   setInterval(function()){
-        seconds--;
-        console.log(seconds);
-        if (seconds === 0){
-            clearInterval(timerInterval);
-            pg2.setAttribute("style", "display:none");
-            console.log(pg2);
-            timesUp.setAttribute("style", "display:block");
-        }
-    }, 1000;
+function startQuiz(){
+    startBtn.style.display="none";
+    quiz.style.display="block";
+    displayQuiz()
 }
 
+function displayQuiz(){
+    problem.innerHTML=buttonquestions[currentQuestion].question
+    multiA.innerText=buttonquestions[currentQuestion].choiceA
+    multiB.innerText=buttonquestions[currentQuestion].choiceB
+    multiC.innerText=buttonquestions[currentQuestion].choiceC
+    multiD.innerText=buttonquestions[currentQuestion].choiceD
 
-//for loop
-for (var i = 0; i < bttonquestions.length; i++) {
-    var response = window.buttonquestions(questions[i].prompt);
-    if (response == questions[i].answer) {
-        score++;
-        alert("Correct!");
-    } else {
-        alert("Incorrect.");
+}
+
+function validateAnswer(){
+    var userchoice = this.getAttribute("id");
+    if(userchoice == buttonquestions[currentQuestion].correct){
+        correct++
     }
+    else{
+        incorrect++
+    }
+    score.innerText="correct:" + correct + " incorrect:" + incorrect
+
+
 }
-alert("you got" + score + " / " + questions.length.numberofquestions)
+// //Trigger the first question after the "Click to Begin"start button is clicked.
+// var answer = "";
+// console.log(answer);
+// var seconds = 60;
+// var correct = 0;
+// var currentQuestion = 0;
+// var timeInterval;
+
+startBtn.addEventListener("click", startQuiz)
+
+   
+// //for timer countdown:
+// setInterval(function () {
+//     timerInterval = setInterval(function () {
+//         seconds--;
+//         clock.display = seconds;
+//         console.log(seconds);
+
+//         if (seconds === 0) {
+//             clearInterval(timerInterval);
+//             pg2.setAttribute("style", "display:none");
+//             console.log(pg2);
+//             timesUp.setAttribute("style", "display:block");
+//         }
+//     }, 1000);
+//         choiceA.addEventListener("click", function(){
+//             answer = "A";
+//             console.log(answer);
+//             checkTheAnswer();
+//             currentQuestion++
+//             setTimeout (function() {displayQuestion();}, 750);
+
+//         })
+
+//         choiceB.addEventListener("click", function(){
+//             answer = "B";
+//             console.log(answer);
+//             checkTheAnswer();
+//             currentQuestion++
+//             setTimeout (function() {displayQuestion();}, 750);
+
+//         })
+
+//         choiceC.addEventListener("click", function(){
+//             answer = "B";
+//             console.log(answer);
+//             checkTheAnswer();
+//             currentQuestion++
+//             setTimeout (function() {displayQuestion();}, 750);
+
+//         })
+    
+//         choiceD.addEventListener("click", function(){
+//             answer = "B";
+//             console.log(answer);
+//             checkTheAnswer();
+//             currentQuestion++
+//             setTimeout (function() {displayQuestion();}, 750);
+
+//         })
+// function displayQuestion(){
+//     console.log(quiz[currentQuestion]);
+//     quizOver();
+//     result.textContent = "";
+//     problem.textContent = quiz[currentQuestion].question;
+//     choiceA.textContent = quiz[currentQuestion].choiceA;
+//     choiceB.textContent = quiz[currentQuestion].choiceB;
+//     choiceC.textContent = quiz[currentQuestion].choiceC;
+//     choiceD.textContent = quiz[currentQuestion].choiceD;
+
+// function checkTheAnswer(){
+//     console.log(answer);
+//     if (quiz[currentQuestion].correct === answer) {
+//         result.textContent = "That's correct!";
+//         correct++;
+//         console.log(correct);
+
+//     }
+//     else{
+//         result.textContent = "That's incorrect!";
+//         seconds = seconds -5;
+//     }
+// }
+// }
+
+// function quizOver(){
+//     if(currentQuestion>quiz.length-1){
+//         clearInterval(timerInterval);
+//         pg2.setAttribute("style", "display:none");
+//         console.log(pg2);
+//         pg3.setAttribute("style", "display:none");
+//         score.textContent = correct + "/5";
+//         extraSeconds.textContent = seconds + " " + "seconds leftover";
+
+//     }
+// }
+     
+//  addEventListener("click", function () {
+//     pg1.setAttribute("style", "display:none");
+    //for loop
+    // for (var i = 0; i < buttonquestions.length; i++) {
+    //     var response = window.buttonquestions(quizQuestions[i].prompt);
+    //     if (response == buttonquestions[i].answer) {
+    //         score++;
+    //         alert("Correct!");
+    //     } else {
+    //         alert("Incorrect.");
